4fe6327b5bdb78a8659ebe237306af1381db07e6
Jeremy Greer <jeremy.greer.atl@gmail.com>
2014-10-10 07:53:25 -0400

    merge client js, update form to accomodate, move js include into main layout

--- public/js/app.js
+++ public/js/app.js
@@ -1,52 +1,54 @@
 $(function () {
     'use strict';
 
-    // TODO: De-dupe all the stuff in here and view.js.
+    var page = window.location.pathname;
     var fields = {};
 
     var msg = function (str) {
         $('.message').html(str);
     };
 
-    // get a reference to each field
-    $('input[type!="submit"]').each(function () {
-        var $this = $(this);
-        fields[$this.attr('id')] = $this;
-    });
-
-    // handle form submission
-    var onSubmit = function () {
-        var data = {};
-
-        $('input').prop('disabled', true);
-
-        $.each(fields, function (fieldName, field) {
-            data[fieldName] = field.val();
+    if (page === '/' || page.match(/\/view/)) {
+        // get a reference to each field
+        $('input[type!="submit"]').each(function () {
+            var $this = $(this);
+            fields[$this.attr('id')] = $this;
         });
 
-        // We don't actually have an _id yet.
-        // Delete it so the backend doesn't try to use "".
-        delete data._id;
+        // initialize form values
+        fields.date.val((new Date()).toISOString().substring(0, 10));
 
-        $.ajax({
-            type: 'POST',
-            url: '/api/run',
-            data: data,
-            error: function () {
-                msg('unable to save run :(');
-            },
-            success: function (run) {
-                msg('run saved');
-                $('button, #view').toggleClass('hidden');
-                $('#view').attr('href', '/view/' + run._id);
+        $('form').on('submit', function () {
+            var $form = $(this);
+            var data = {};
+
+            $('input').prop('disabled', true);
+
+            $.each(fields, function (fieldName, field) {
+                data[fieldName] = field.val();
+            });
+
+            // We don't actually have an _id yet.
+            // Delete it so the backend doesn't try to use "".
+            if (page === '/') {
+                delete data._id;
             }
+
+            $.ajax({
+                type: $form.attr('method'),
+                url: $form.attr('action'),
+                data: data,
+                error: function () {
+                    msg('unable to save run :(');
+                },
+                success: function (run) {
+                    msg('run saved');
+                    $('button, #view').toggleClass('hidden');
+                    $('#view').attr('href', '/view/' + run._id);
+                }
+            });
+
+            return false;
         });
-
-        return false;
-    };
-
-    // initialize form values
-    fields.date.val((new Date()).toISOString().substring(0, 10));
-
-    $('form').on('submit', onSubmit);
+    }
 }());
